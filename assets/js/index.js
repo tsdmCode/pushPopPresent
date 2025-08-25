030251f62863aea86a0d4c2b287ab67211cf53c5
tsdmCode <116952397+tsdmCode@users.noreply.github.com>
2025-08-25 10:39:48 +0200

    Satte lidt kommentarer på for at være mere brugervenlig

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,24 +4,26 @@ const memory = document.getElementById('memory');
 const popBtn = document.getElementById('pop');
 const pushBtn = document.getElementById('push');
 let popped;
+
 const arr = [45, 20, 600];
 
 function updateArr() {
+  //den her funktion opdaterer bare hjemmesiden efter vi har trykket på knappen
   arrKasse.innerText = arr.join(', ');
   length.innerText = `Længden på arrayet er: ${arr.length}`;
   memory.innerText = `Sidste poppede nummer er: ${popped || ''}`;
 }
 
 function pushNum() {
-  const nytItem = prompt('Hvad vil du gerne tilføje?');
-  console.log('Adding ' + nytItem);
-  arr.push(nytItem);
+  const nytItem = prompt('Hvad vil du gerne tilføje?'); //her tager vi brugerens input (det var egentlig et random tal i starten)
+  console.log('Tilføjer ' + nytItem);
+  arr.push(nytItem); //her tager vi brugerens input fra nytitem og "lægger det til" vores array
 
   updateArr();
 }
 
 function popNum() {
-  popped = arr.pop();
+  popped = arr.pop(); //den her linje kigger bare på den sidste ting i vores array, fjerner det, og gemmer det til senere brug
   console.log(popped);
 
   updateArr();
